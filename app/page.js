33c77aef15418f78a6c68026a56983022a303c72
guoluoqian <24037915@myrp.edu.sg>
2025-12-11 14:20:48 +0800

    cat-fish game andquiz jsx

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import footer from '../components/footer';
 import game1 from '../components/game1';
 import game2 from '../components/game2';
 import navbar from '../components/navbar';
-import quiz from '../components/quiz';
+import quiz from '../components/quiz.jsx';
 import welcome from '../components/welcome';
 
 
